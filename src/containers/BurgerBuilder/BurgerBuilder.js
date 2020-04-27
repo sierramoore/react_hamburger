770f5d50411346889407c2c833709e8e71d427d2
Sierra Moore <sierra.v.moore@gmail.com>
2020-04-27 17:05:27 +0200

    imported axioios and sent post req to firebase server

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -5,6 +5,8 @@ import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
 
+import axios from '../../axios-orders';
+
 
 const Ingredient_Prices = {
     salad: 0.5,
@@ -94,7 +96,25 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('you continue')
+        // alert('you continue')
+        const order = {
+            ingredients: this.state.ingredients,
+            price: this.state.totalPrice,
+            customer: {
+                name: 'Sierra',
+                address: {
+                    street: 'calle uno',
+                    zipCode: '1234',
+                    country: 'Spain'
+                },
+                email: 'me@me.com'
+            },
+            deliveryMethod: 'fastest'
+        }
+        axios.post('/orders.json', order)
+            .then(response => console.log(response))
+            .catch(error => console.log(error))
+        // node_name_of_ur_choice then .json and second param is data u want to pass
     }
 
     render() {
